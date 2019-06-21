4d2fdc24e2568c8e7a23c7c51b40ccb49c37e21e
Chris Killian <killiantheelder@gmail.com>
2019-06-21 10:52:45 -0700

    Commit of new capsule

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(Time.deltaTime * 10f, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(Time.deltaTime * -10f, 0);
+        }
+    }
+}
